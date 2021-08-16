4e0f0c7daeb85c1d2013efbc6d839cc9eb47fe22
moayed-mustafa <moayed.abrida@yahoo.com>
2021-08-17 00:34:10 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "./custom.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Crowd funding App</h1>
+
+    </div>
+  );
+}
+
+export default App;
